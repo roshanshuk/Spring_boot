97978dc31bdf91651a0f918dd973f6cf56958564
roshanshuk <roshanshukla67@gmail.com>
2022-07-12 14:30:13 +0530

    Spring boot demo

---
+++ SpringBoot_demo/src/main/java/dev/roshan/SpringDemoApplication.java
@@ -0,0 +1,14 @@
+package dev.roshan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDemoApplication.class, args);
+		System.out.println("Hello Spring Boot");
+	}
+
+}
